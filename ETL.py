f3d844a25a6bc7df44cc041dd114f179e280d2c1
marcoa0602 <marcoa0602@gmail.com>
2023-08-17 12:35:29 -0400

    changes

--- ETL.py
+++ ETL.py
@@ -1,40 +1,37 @@
 import pandas as pd
 import os
 
-def extract_from_csv(archivo):
-    dataframe = pd.read_csv(archivo)
+def extract(ruta_datos):
+    dataframe = pd.read_csv(ruta_datos)
+
     return dataframe
 
-def extract(ruta_datos):
-    data = extract_from_csv(ruta_datos)
+def transform_gender(dataframe):
+    dataframe.drop(columns = ['ID', 'Dream Weight'], inplace = True)
+
+    gender_dummy = pd.get_dummies(dataframe['Gender'], prefix='Gender', drop_first = True)
+    data = pd.concat([dataframe, gender_dummy], axis=1).drop(columns = ['Gender'])
 
     return data
 
-def transform(ruta_datos):
-    data = pd.read_csv(ruta_datos)
-    
-    data.drop(columns = ['ID', 'Dream Weight'], inplace = True)
-
-    gender_dummy = pd.get_dummies(data['Gender'], prefix='Gender', drop_first = True)
-    data = pd.concat([data, gender_dummy], axis=1).drop(columns = ['Gender'])
-
-    
+def transform_weather(data):
     weather_dummy = pd.get_dummies(data['Weather Conditions'], prefix='Weather Conditions', drop_first = True)
     data = pd.concat([data, weather_dummy], axis=1).drop(columns = ['Weather Conditions'])
 
     return data
 
 def load(data):
-    archive_path = 'PR2\PR2\Datos'
+    archive_path = 'C:\PR2\PR2\Datos'
     csv_path = 'datos_transformados'
     output_path = os.path.join(archive_path, csv_path)
+    output_path = data.to_csv(output_path, index = "False")
 
-    data.to_csv(output_path, index = "False")
     return output_path
 
-def tuberia_de_datos(ruta_datos):
-    datos_crudos = extract(ruta_datos)
-    datos_trans = transform(datos_crudos)
-    carga = load(datos_trans)
+def tuberia_de_datos(datos):
+    datos_crudos = extract(datos)
+    datos_trans1 = transform_gender(datos_crudos)
+    datos_trans2 = transform_weather(datos_trans1)
+    carga = load(datos_trans2)
 
     return carga
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import pandas as pd
 
 url = 'https://drive.google.com/uc?id=1dnvro-P6hLJQ07oWBjFWXdTv6KLTxzkk'
-df_exercise = pd.read_csv(url)
-df_exercise
 
 from ETL import tuberia_de_datos
 
-tuberia_de_datos(df_exercise)
+df_tuberia = tuberia_de_datos(url)
+df_tuberia.head()
