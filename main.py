ae63804a0f2aac024163bfc923f4211666b939a4
marcoa0602 <marcoa0602@gmail.com>
2023-08-17 13:57:53 -0400

    cambios url

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-url = 'https://raw.githubusercontent.com/marcoa0602/PR2/main/Datos/datos_brutos.csv?token=GHSAT0AAAAAACF4WCPNC4XFSRPO72TSEA7SZG6LEVQ'
+url = 'https://drive.google.com/uc?id=1dnvro-P6hLJQ07oWBjFWXdTv6KLTxzkk'
 
 from ETL import tuberia_de_datos
 tuberia = tuberia_de_datos(url)
