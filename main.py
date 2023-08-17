46614af349ca63af65740ac354e4a6f5e2e00c3a
marcoa0602 <marcoa0602@gmail.com>
2023-08-17 13:31:34 -0400

    agregado a main.py

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
+url = 'https://raw.githubusercontent.com/marcoa0602/PR2/main/Datos/datos_brutos.csv?token=GHSAT0AAAAAACF4WCPNC4XFSRPO72TSEA7SZG6LEVQ'
 
+from ETL import tuberia_de_datos
+tuberia = tuberia_de_datos(url)
